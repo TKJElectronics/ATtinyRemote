33798c0a0247f3dfc24ad2a43349af6723115cb3
Kristian Sloth Lauszus <lauszus@gmail.com>
2015-08-06 00:46:08 +0200

    Fixed formatting
    No code change

--- ATtinyRemoteVer2/ATtinyRemoteVer2.ino
+++ ATtinyRemoteVer2/ATtinyRemoteVer2.ino
@@ -78,15 +78,15 @@ void setup() {
   DDRB |= _BV(IRLED); // Set as output
   PORTB &= ~(_BV(IRLED)); // When not sending PWM, we want it low
   DDRB &= ~(_BV(IRRECV)); // Set as input
-  
+
   /* Disable Analog to Digitalconverter to save power */
   ADCSRA &= ~(_BV(ADEN));
 
   /* Set up timer0 to "Clear Timer on Compare Match (CTC) Mode" at a 6ms interval */
   TCCR0A = _BV(WGM01); // Set "Clear Timer on Compare Match (CTC) Mode" - OCR0A is TOP
-  TCCR0B = _BV(CS02)| _BV(CS00); // Set precscaler to 1024
+  TCCR0B = _BV(CS02) | _BV(CS00); // Set precscaler to 1024
   OCR0A = TIMERVALUE; // Set the value, so there is a compare match every 6ms
-  TIMSK = _BV(OCIE0A); //  Timer/Counter0 Output Compare Match A Interrupt Enable   
+  TIMSK = _BV(OCIE0A); //  Timer/Counter0 Output Compare Match A Interrupt Enable
 
   /* Set up external interrupt on pin 7 */
   MCUCR |= _BV(ISC01); // The falling edge of INT0 generates an interrupt request
@@ -95,89 +95,89 @@ void setup() {
 
   /* Enable PWM with a frequency of 38kHz on pin 6 (OC1A) */
   TCCR1 = _BV(PWM1A) | _BV(CS12); // Enable PWM and set prescaler to 8
-  OCR1C = (((F_CPU/8/PWMFREQUENCY))-1); // Set PWM Frequency to 38kHz, OCR1C is TOP, see the datasheet page 90
-  OCR1A = OCR1C/3; // 33% duty cycle
+  OCR1C = (((F_CPU / 8 / PWMFREQUENCY)) - 1); // Set PWM Frequency to 38kHz, OCR1C is TOP, see the datasheet page 90
+  OCR1A = OCR1C / 3; // 33% duty cycle
 
   /* Enable watchdog timer at 4 seconds */
   wdt_enable(WDTO_4S);
-  
-  sei(); // Enables interrupts by setting the global interrupt mask  
+
+  sei(); // Enables interrupts by setting the global interrupt mask
 
   /* Indicate startup */
   PORTB |= _BV(LED); // Turn LED on
   newDelay(100);
-  PORTB &= ~(_BV(LED)); // Turn LED off  
+  PORTB &= ~(_BV(LED)); // Turn LED off
 }
 
 void loop() {
   wdt_reset(); // Reset watchdog timer
-  
-  if(finishedReading) {
-    if((uint16_t)(IRData >> 32) == PanasonicAddress) {   
-      if(deactivated) {
-        if((uint32_t)IRData == ATtinyPower) {
+
+  if (finishedReading) {
+    if ((uint16_t)(IRData >> 32) == PanasonicAddress) {
+      if (deactivated) {
+        if ((uint32_t)IRData == ATtinyPower) {
           deactivated = false;
-          PORTB &= ~(_BV(LED)); // Turn LED off          
-          newDelay(250); // delay insures that it's just don't toggle "deactivate" very fast          
-        }        
+          PORTB &= ~(_BV(LED)); // Turn LED off
+          newDelay(250); // delay insures that it's just don't toggle "deactivate" very fast
+        }
         finishedReading = 0; // Clear flag
       }
       else {
-        switch((uint32_t)IRData) {
-        case PanasonicVolumeUp:   
-          JVCCommand(JVCVolumeUp,0);        
-          break;
-        case PanasonicVolumeDown:    
-          JVCCommand(JVCVolumeDown,0);
-          break;
-        case PanasonicMute:
-          JVCCommand(JVCMute,1);
-          break;
-        case PanasonicPower:
-          JVCCommand(JVCPower,2);
-          newDelay(3000); // On Panasonic TVs one have to hold the power button to turn the TV on, this delay keeps the ATtinyRemote from toggling the JVC stereo on and off
-          finishedReading = 0; // Clear flag
-          break;
-        case PanasonicPower2:
-          JVCCommand(JVCPower,1);
-          break;
-        case ATtinyPower: // This will disable the ATtinyRemote until the button is pressed again
-          deactivated = true;
-          PORTB |= _BV(LED); // Turn LED on          
-          newDelay(250); // delay insures that it's just don't toggle "deactivate" very fast
-          finishedReading = 0; // Clear flag
-          break;
-        default:
-          finishedReading = 0; // Clear flag
-          break;     
+        switch ((uint32_t)IRData) {
+          case PanasonicVolumeUp:
+            JVCCommand(JVCVolumeUp, 0);
+            break;
+          case PanasonicVolumeDown:
+            JVCCommand(JVCVolumeDown, 0);
+            break;
+          case PanasonicMute:
+            JVCCommand(JVCMute, 1);
+            break;
+          case PanasonicPower:
+            JVCCommand(JVCPower, 2);
+            newDelay(3000); // On Panasonic TVs one have to hold the power button to turn the TV on, this delay keeps the ATtinyRemote from toggling the JVC stereo on and off
+            finishedReading = 0; // Clear flag
+            break;
+          case PanasonicPower2:
+            JVCCommand(JVCPower, 1);
+            break;
+          case ATtinyPower: // This will disable the ATtinyRemote until the button is pressed again
+            deactivated = true;
+            PORTB |= _BV(LED); // Turn LED on
+            newDelay(250); // delay insures that it's just don't toggle "deactivate" very fast
+            finishedReading = 0; // Clear flag
+            break;
+          default:
+            finishedReading = 0; // Clear flag
+            break;
         }
-      }       
+      }
 #ifdef SLEEP // This can be enabled, but I decided not to, as it will miss the first command sent from the remote
       wdt_disable(); // Disable watchdog timer before going to sleep
       sleep(); // Put the device into sleep mode - the IR Receiver will trigger a external interrupt and wake the device
       wdt_enable(WDTO_4S); // Reenable watchdog timer
 #endif
-    } 
+    }
     else
       finishedReading = 0; // Clear flag
-  }  
+  }
 }
 
 void newDelay(uint16_t time) { // I use my own simple delay, as I can't use delay() as it uses timer0 - delayMicroseconds() still work, as it uses assembly language, see https://github.com/arduino/Arduino/blob/master/hardware/arduino/cores/arduino/wiring.c
-  compareMatchCounter = time/6; // Set number of counts - The input is in ms and the resolution is 6ms
-  TCNT0 = 0; // Reset timer 
-  while(compareMatchCounter);
+  compareMatchCounter = time / 6; // Set number of counts - The input is in ms and the resolution is 6ms
+  TCNT0 = 0; // Reset timer
+  while (compareMatchCounter);
 }
 
 void JVCCommand(uint16_t data, uint8_t waitForSend) {
   /* Don't wait until finished sending command before decoding next incomming data */
-  if(!waitForSend)    
+  if (!waitForSend)
     finishedReading = 0; // Clear flag
 
   /* Send the command */
   uint16_t dataTemp; // Used to store data
-  uint8_t nrRepeats; // Store the number of repeats  
-  if(data == JVCVolumeUp || data == JVCVolumeDown) // Only send one repeat if it's a volume up or down command
+  uint8_t nrRepeats; // Store the number of repeats
+  if (data == JVCVolumeUp || data == JVCVolumeDown) // Only send one repeat if it's a volume up or down command
     nrRepeats = 1;
   else
     nrRepeats = 5;
@@ -188,83 +188,83 @@ void JVCCommand(uint16_t data, uint8_t waitForSend) {
   SPACE
   delayMicroseconds(JVC_HDR_SPACE);
 
-  /* Send body */  
-  for(uint8_t i = 0; i <= nrRepeats; i++) {
+  /* Send body */
+  for (uint8_t i = 0; i <= nrRepeats; i++) {
     dataTemp = data; // Store data
-    for (uint8_t j = 0; j < 16; j++) {      
+    for (uint8_t j = 0; j < 16; j++) {
       MARK
       delayMicroseconds(JVC_BIT_MARK);
       SPACE
-      if (dataTemp & 0x8000) // The data is send MSB      
-        delayMicroseconds(JVC_ONE_SPACE); 
-      else    
+      if (dataTemp & 0x8000) // The data is send MSB
+        delayMicroseconds(JVC_ONE_SPACE);
+      else
         delayMicroseconds(JVC_ZERO_SPACE);
       dataTemp <<= 1; // Shift data
     }
     MARK // Stop Mark
     delayMicroseconds(JVC_BIT_MARK);
-    SPACE // Turn IR LED off  
-    if(i != nrRepeats) // Don't make a delay, if it's the last repeat
+    SPACE // Turn IR LED off
+    if (i != nrRepeats) // Don't make a delay, if it's the last repeat
       newDelay(20); // Wait 20 ms before sending again - this is actually not very precise if the flag has been reset, as the interrupt might reset the timer, but it works anyway
   }
-  
+
   /* Wait until finished sending command before decoding next incomming data */
-  if(waitForSend == 1)    
+  if (waitForSend == 1)
     finishedReading = 0; // Clear flag
-    
+
   // waitForSend == 2, then it will not reset the data and set clear flag
 }
-/* 
+/*
   A mark sends an IR mark for the specified number of microseconds.
   The mark output is modulated at the PWM frequency.
 
   TCCR1 |= _BV(COM1A1);
-  This will enable the pwm output:  
-  OC1A cleared on compare match (TCNT1 = OCR1A). Set when TCNT1 = 0. 
+  This will enable the pwm output:
+  OC1A cleared on compare match (TCNT1 = OCR1A). Set when TCNT1 = 0.
 
   A space sends an IR space for the specified number of microseconds.
   A space is no output, so the PWM output is disabled.
-   
-  TCCR1 &= ~(_BV(COM1A1));  
-  This wil disable the pwm output:  
-  OC1A not connected.  
+
+  TCCR1 &= ~(_BV(COM1A1));
+  This wil disable the pwm output:
+  OC1A not connected.
 */
 
-ISR(INT0_vect) { // External interrupt at INT0  
-  if(!finishedReading) { // Wait until the data has been read
-    switch(IRState) {
-    case 0:
-      if(compareMatch) // First pulse after long pause
-        IRState = 1;
-      break;
-    case 1:
-      if(!compareMatch && TCNT0 > headerTime) { // Check if the pulse is not to long and it is longer than the headerTime
-        currentPulse = 0; // Reset pulse counter
-        IRBuffer = 0; // Reset buffer
-        IRState = 2;
-      } 
-      else
-        IRState = 0;
-      break;
-    case 2:
-      if(compareMatch) // To long a pause, so it must be a gap
-        IRState = 0;
-      else {
-        if(TCNT0 < spaceTime) // Check if it's a mark
-          IRBuffer <<= 1;       
-        else // It must be a space
-          IRBuffer = (IRBuffer << 1) | 1;
-        currentPulse++;          
-        if (currentPulse == PANASONIC_BITS) { // All bits have been received
-          IRData = IRBuffer;
-          finishedReading = 1; // Indicate that the reading is finished, this has to be cleared in software
-          IRState = 0; // Reset IR state for next command
-        }                
-      }
-      break;
+ISR(INT0_vect) { // External interrupt at INT0
+  if (!finishedReading) { // Wait until the data has been read
+    switch (IRState) {
+      case 0:
+        if (compareMatch) // First pulse after long pause
+          IRState = 1;
+        break;
+      case 1:
+        if (!compareMatch && TCNT0 > headerTime) { // Check if the pulse is not to long and it is longer than the headerTime
+          currentPulse = 0; // Reset pulse counter
+          IRBuffer = 0; // Reset buffer
+          IRState = 2;
+        }
+        else
+          IRState = 0;
+        break;
+      case 2:
+        if (compareMatch) // To long a pause, so it must be a gap
+          IRState = 0;
+        else {
+          if (TCNT0 < spaceTime) // Check if it's a mark
+            IRBuffer <<= 1;
+          else // It must be a space
+            IRBuffer = (IRBuffer << 1) | 1;
+          currentPulse++;
+          if (currentPulse == PANASONIC_BITS) { // All bits have been received
+            IRData = IRBuffer;
+            finishedReading = 1; // Indicate that the reading is finished, this has to be cleared in software
+            IRState = 0; // Reset IR state for next command
+          }
+        }
+        break;
     }
     TCNT0 = 0; // Clear timer, don't clear if the data is been read, as the timer is used by newDelay()
-  }  
+  }
   compareMatch = 0; // Clear flag
 }
 ISR(TIM0_COMPA_vect) { // Timer0/Counter Compare Match A
@@ -276,17 +276,17 @@ ISR(TIM0_COMPA_vect) { // Timer0/Counter Compare Match A
 void sleep() { // The ATtiny85 is woken by a external interrupt on INT0 from the IR REceiver
   //PORTB &= ~(_BV(LED)); // Turn off LED when it goes to sleep
 
-  // The falling edge of INT0 generates an interrupt request. 
-  MCUCR &= ~(_BV(ISC00) | _BV(ISC01)); // To wake up from Power-down, only level interrupt for INT0 can be used.    
+  // The falling edge of INT0 generates an interrupt request.
+  MCUCR &= ~(_BV(ISC00) | _BV(ISC01)); // To wake up from Power-down, only level interrupt for INT0 can be used.
 
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Set sleep mode
   sleep_mode(); // Here the device is put to sleep
-  
+
   TCNT0 = 0; // Clear timer
   compareMatch = 0; // Clear flag
-  
+
   // Disable level interrupt and set back to falling edge interrupt
-  MCUCR |= _BV(ISC01); // The falling edge of INT0 generates an interrupt request 
+  MCUCR |= _BV(ISC01); // The falling edge of INT0 generates an interrupt request
 
   //PORTB |= _BV(LED); // Turn it back on
 }
